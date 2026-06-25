79174a6f58968be3ca9cf69e68abf25a38b15e19
brunoalekohler <bruno.aprendiz.sr@gmail.com>
2026-06-25 15:38:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // =====================================
 
 // URL da API do Google Apps Script
-const API_URL = "https://script.google.com/macros/s/AKfycbzZGoiXDhzDPprGeBcCcGZ1yKRL11YNYXuNdNM9a42hHoeKxPATYDlhgFhxPGZ6eHBH/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzouh_pD3vLSwT7IqVCOo1cchorlBeWo_2eqI2h-5rfbrgGp6q-of-Rxr94m4hUPvCK/exec";
 
 const codigo = document.getElementById("codigo");
 const parametros = new URLSearchParams(window.location.search);
